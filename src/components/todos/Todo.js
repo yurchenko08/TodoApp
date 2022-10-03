5429867b292c78931458096cb1066fd891ddda81
yurchenko08 <yurchenko08@gmail.com>
2022-10-03 19:41:47 +0300

    added styles, icon

--- src/components/todos/Todo.js
+++ src/components/todos/Todo.js
@@ -1,8 +1,11 @@
 import React from "react";
+import { RiTodoFill } from "react-icons/ri";
+import style from "./Todo.module.css";
 export default function Todo({ todo, index, deleteTodo }) {
   return (
-    <div onDoubleClick={() => deleteTodo(index)}>
-      <div>{todo}</div>
+    <div onDoubleClick={() => deleteTodo(index)} className={style.todo}>
+      <RiTodoFill className={style.todoIcon} />
+      <div className={style.todoText}>{todo}</div>
     </div>
   );
 }
--- src/components/todos/TodoList.js
+++ src/components/todos/TodoList.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Todo from "./Todo";
+import style from "./TodoList.module.css";
 function TodoList({ todos, deleteTodo }) {
   return (
-    <>
+    <div className={style.todoListContainer}>
       {!todos.length && <h3>Todo list is empty</h3>}
       {todos.map((todo, index) => (
         <Todo key={index} todo={todo} index={index} deleteTodo={deleteTodo} />
       ))}
-    </>
+    </div>
   );
 }
 export default TodoList;
